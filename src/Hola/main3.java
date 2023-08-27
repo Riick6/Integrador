fec475248435e2a666604130a616d411e1616f81
Rick <rick6.uwu@gmail.com>
2023-08-27 17:29:56 -0500

    Se agrega el proyecto

---
+++ src/Hola/main3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Hola;
+
+/**
+ *
+ * @author CARSA - PISCO
+ */
+public class main3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
